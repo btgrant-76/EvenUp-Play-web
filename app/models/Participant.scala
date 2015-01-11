ab0b3f4fc5689d9dfa4531f0a32492ddd5908c62
Brian T. Grant <brian.grant@morningstar.com>
2015-01-11 13:28:12 -0600

    Participant equality is based on name only.

--- app/models/Participant.scala
+++ app/models/Participant.scala
@@ -1,9 +1,26 @@
 package models
 
+import org.apache.commons.lang3.StringUtils
 import play.api.cache.Cache
 import play.api.Play.current
 
-class Participant(val name: String, val expenses: Seq[Expense] = Seq())
+class Participant(val name: String, val expenses: Seq[Expense] = Seq()) {
+  assume(StringUtils.isNotBlank(name))
+
+  def canEqual(other: Any): Boolean = other.isInstanceOf[Participant]
+
+  override def equals(other: Any): Boolean = other match {
+    case that: Participant =>
+      (that canEqual this) &&
+      name == that.name
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(name)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
+}
 
 object Participant {
   val SESSION_KEY = "session.participants"
